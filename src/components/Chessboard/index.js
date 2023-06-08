8eb415bc199805513b2a010fee1d8e0c81d6141a
Carlos Pessoa <carlos.henriquepessoa15@gmail.com>
2023-06-08 15:30:14 -0300

    adicionado key no map das coordenadas do board

--- src/components/Chessboard/index.js
+++ src/components/Chessboard/index.js
@@ -5,22 +5,27 @@ const horizontal = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
 function Chessboard() {
     let board = [];
+    let key = 0;
 
     for (let y = (vertical.length - 1); y >= 0; y--) {
         for (let i = 0; i < horizontal.length; i++) {
             board.push({
+                id: key,
                 name: `|${horizontal[i]} ${vertical[y]}|`,
                 number: (i + y + 2) % 2
             });
+            key++
         }
     }
 
     return (
         <div className='container-board'>
             {board.map((coord) => (
-                <div className={`${coord.number === 0
-                    ? 'tile-black'
-                    : 'tile-white'} 
+                <div
+                    key={coord.id}
+                    className={`${coord.number === 0
+                        ? 'tile-black'
+                        : 'tile-white'} 
                     tile`}>
                     {coord.name}
                 </div>
