b19d7e59b3eb79a152e2bb028714274e2aae9471
Carlos Pessoa <carlos.henriquepessoa15@gmail.com>
2023-06-21 09:25:20 -0300

    Peões adicionados ao tabuleiro

--- src/components/Chessboard/index.js
+++ src/components/Chessboard/index.js
@@ -1,8 +1,20 @@
 import Tile from '../Tile';
 import './styles.css';
+import PawnB from '../../assets/ChessPawnB.png';
+import PawnW from '../../assets/ChessPawnW.png'
 
 const vertical = ['1', '2', '3', '4', '5', '6', '7', '8'];
 const horizontal = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+const pieces = [];
+
+for (let i = 0; i < 8; i++) {
+    pieces.push({ image: PawnB, h: i, v: 6 });
+}
+
+for (let i = 0; i < 8; i++) {
+    pieces.push({ image: PawnW, h: i, v: 1 });
+}
 
 function Chessboard() {
     let board = [];
@@ -10,10 +22,18 @@ function Chessboard() {
 
     for (let y = (vertical.length - 1); y >= 0; y--) {
         for (let i = 0; i < horizontal.length; i++) {
+            let image;
+            pieces.forEach((p) => {
+                if (p.h === i && p.v === y) {
+                    image = p.image;
+                }
+            })
+
             board.push({
                 id: key,
                 name: `|${horizontal[i]} ${vertical[y]}|`,
-                number: (i + y + 2) % 2
+                number: (i + y + 2) % 2,
+                image: image
             });
             key++
         }
@@ -25,6 +45,7 @@ function Chessboard() {
                 <Tile
                     key={coord.id}
                     number={coord.number}
+                    piece={coord.image}
                 />
             ))}
         </div>
--- src/components/Tile/index.js
+++ src/components/Tile/index.js
@@ -1,12 +1,15 @@
 import './styles.css';
 
-function Tile({ number }) {
+function Tile({ number, piece }) {
     return (
         <div
             className={`${number === 0
                 ? 'tile-black'
                 : 'tile-white'} 
         tile`}>
+            {piece &&
+                <img src={piece} alt='piece' />
+            }
         </div>
     )
 }
